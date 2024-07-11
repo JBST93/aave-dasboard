e29109ec9b72cb2b45919fe344d47f5a94c21341
Jacek Bastin <jacek.bastin@gmail.com>
2024-07-11 16:43:51 +0200

    Serve React app with Flask

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000', // Use environment variable or default to localhost for development
+  baseURL: import.meta.env.REACT_APP_API_URL, // Use the environment variable
 });
 
 export default instance;
