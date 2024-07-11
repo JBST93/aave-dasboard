d8800023bdc68a18d14e89542d7ce598d9b2cbf4
Jacek Bastin <jacek.bastin@gmail.com>
2024-07-11 16:46:56 +0200

    Serve React app from Flask

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import FetchData from './components/FetchData';
-import Footer from './components/Footer';
-import Statistics from './components/Statistics';
-
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossorigin
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-        />
-        <h1>Money Market Rates</h1>
-        <FetchData />
-        <Footer />
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
