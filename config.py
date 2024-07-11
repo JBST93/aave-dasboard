b310c49cdb521f8691bd51c2c42c67fd24a5cdfc
Jacek Bastin <jacek.bastin@gmail.com>
2024-07-11 23:01:39 +0200

    Set environment variables for database connections

--- config.py
+++ config.py
@@ -7,20 +7,19 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL2').replace("postgres://", "postgresql://")
+
 
 class ProductionConfig(Config):
     DEVELOPMENT = False
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL2').replace("postgres://", "postgresql://")
 
 
 class StagingConfig(Config):
     DEVELOPMENT = False
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL_DEV').replace("postgres://", "postgresql://")
 
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL_DEV').replace("postgres://", "postgresql://")
