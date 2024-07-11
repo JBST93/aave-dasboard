151f3e7490987a7703427531bcd79a9bedc2516a
Jacek Bastin <jacek.bastin@gmail.com>
2024-07-11 17:36:03 +0200

    Fixing render static page

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensure the database and tables are created
         scheduler = start_scheduler()
-    app.run(debug=True)
+    app.run(debug=False)
