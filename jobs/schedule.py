a823f3fd45d28906d55b1f0489b0483293de54cb
Jacek Bastin <jacek.bastin@gmail.com>
2024-07-14 22:25:10 +0200

    Fixed Morpho

--- jobs/schedule.py
+++ jobs/schedule.py
@@ -10,7 +10,7 @@ from curve.fetch_store_data import fetch_store_data as curve
 from gearbox.fetch_data import fetch_store_rates as gearbox
 from spark.fetch_rates import fetch_store_rates as maker_dsr
 from compound.fetch_rates import fetch_store_rates as compound
-from morpho.fetch_data_morpho import fetch_data_morpho as morpho
+from morpho.fetch_data_morpho import fetch_data as morpho
 
 
 # Setup logging
@@ -19,44 +19,53 @@ logger = logging.getLogger(__name__)
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=5)
 def fetch_store_data():
-    try:
-        logger.info("Fetching Data for Aave")
-        aave()
-        logger.info("Aave Data fetched")
-    except Exception as e:
-        logger.error(f"Error fetching Aave data: {e}")
+    with app.app_context():
+        try:
+            logger.info("Fetching Data for Aave")
+            aave()
+            logger.info("Aave Data fetched")
+        except Exception as e:
+            logger.error(f"Error fetching Aave data: {e}")
 
-    try:
-        logger.info("Fetching Data for Curve")
-        curve()
-        logger.info("Curve Data fetched")
-    except Exception as e:
-        logger.error(f"Error fetching Curve data: {e}")
+        try:
+            logger.info("Fetching Data for Curve")
+            curve()
+            logger.info("Curve Data fetched")
+        except Exception as e:
+            logger.error(f"Error fetching Curve data: {e}")
 
-    try:
-        logger.info("Fetching Data for Gearbox")
-        gearbox()
-        logger.info("Gearbox Data fetched")
-    except Exception as e:
-        logger.error(f"Error fetching Gearbox data: {e}")
+        try:
+            logger.info("Fetching Data for Gearbox")
+            gearbox()
+            logger.info("Gearbox Data fetched")
+        except Exception as e:
+            logger.error(f"Error fetching Gearbox data: {e}")
 
-    try:
-        logger.info("Fetching Data for Maker DSR")
-        maker_dsr()
-        logger.info("Maker DSR Data fetched")
-    except Exception as e:
-        logger.error(f"Error fetching Maker DSR data: {e}")
+        try:
+            logger.info("Fetching Data for Maker DSR")
+            maker_dsr()
+            logger.info("Maker DSR Data fetched")
+        except Exception as e:
+            logger.error(f"Error fetching Maker DSR data: {e}")
 
-    try:
-        logger.info("Fetching Data for Compound")
-        compound()
-        logger.info("Compound Data fetched")
-    except Exception as e:
-        logger.error(f"Error fetching Compound data: {e}")
+        try:
+            logger.info("Fetching Data for Compound")
+            compound()
+            logger.info("Compound Data fetched")
+        except Exception as e:
+            logger.error(f"Error fetching Compound data: {e}")
 
+        try:
+            logger.info("Fetching Data for Morpho")
+            morpho()
+            logger.info("Morpho Data fetched")
+        except Exception as e:
+            logger.error(f"Error fetching Morpho data: {e}")
 
-    logger.info("Data fetching completed")
+        logger.info("Data fetching completed")
 
-sched.start()
+sched.add_job(fetch_store_data, 'interval', minutes=30)
+
+if __name__ == '__main__':
+    sched.start()
--- morpho/fetch_data_morpho.py
+++ morpho/fetch_data_morpho.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 import sys, os
+import logging
 
 # Ensure the root directory is in the Python path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -12,16 +13,18 @@ from instances.MoneyMarketRate import MoneyMarketRate
 # Define the endpoint and headers
 url = 'https://blue-api.morpho.org/graphql'
 headers = {
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Connection': 'keep-alive',
-        'DNT': '1',
-        'Origin': 'https://blue-api.morpho.org'
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Origin': 'https://blue-api.morpho.org'
 }
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def fetch_data():
+def fetch_data_morpho():
     query = """
     query {
       markets {
@@ -50,7 +53,7 @@ def fetch_data():
             fee
             utilization
           }
-        morphoBlue {
+          morphoBlue {
             chain {
               id
               network
@@ -68,91 +71,87 @@ def fetch_data():
     if response.status_code == 200:
         data = response.json().get("data", {}).get("markets", {}).get("items", [])
         with app.app_context():
-
             for market in data:
-                if market["state"]["supplyAssetsUsd"]:
-                    try:
+                try:
+                    if market and market["state"] and market["state"].get("supplyAssetsUsd"):
                         protocol = "Morpho Blue"
                         supply_token = market["loanAsset"]["symbol"]
-                        collateral_token = market["collateralAsset"]["symbol"] if market["collateralAsset"] else ""
-                        supply_apy = market["state"]['supplyApy']
-                        borrow_apy = market["state"]['borrowApy']
-                        supply_amount = market["state"]["supplyAssetsUsd"]
+                        collateral_token = market["collateralAsset"]["symbol"] if market.get("collateralAsset") else ""
+                        supply_apy = market["state"].get('supplyApy', 0)
+                        borrow_apy = market["state"].get('borrowApy', 0)
+                        supply_amount = market["state"].get("supplyAssetsUsd", 0)
                         chain = market["morphoBlue"]["chain"]["network"]
 
                         rate = MoneyMarketRate(
-                                token=supply_token,
-                                collateral=collateral_token,
-                                protocol="Morpho Blue",
-                                liquidity_rate=supply_apy*100,
-                                liquidity_reward_rate=None,
-                                chain=chain.capitalize(),
-                                borrow_rate=borrow_apy,
-                                tvl=supply_amount,
-                                timestamp=datetime.utcnow(),
-                            )
+                            token=supply_token,
+                            collateral=collateral_token,
+                            protocol="Morpho Blue",
+                            liquidity_rate=supply_apy * 100,
+                            liquidity_reward_rate=None,
+                            chain=chain.capitalize(),
+                            borrow_rate=borrow_apy,
+                            tvl=supply_amount,
+                            timestamp=datetime.utcnow(),
+                        )
 
                         db.session.add(rate)
-
-                    except KeyError as e:
-                        print(f"{e}")
-
+                except Exception as e:
+                    logger.error(f"Error processing market data: {e}")
             db.session.commit()
-
     else:
-        print(f"Query failed to run with a {response.status_code}.")
+        logger.error(f"Query failed to run with a {response.status_code}.")
 
 def fetch_data_metamorpho():
     query = """
     query {
-        vaults {
-            items {
+      vaults {
+        items {
+          address
+          symbol
+          name
+          creationBlockNumber
+          creationTimestamp
+          creatorAddress
+          whitelisted
+          asset {
+            id
             address
-            symbol
-            name
-            creationBlockNumber
-            creationTimestamp
-            creatorAddress
-            whitelisted
-            asset {
-                id
-                address
-                decimals
-            }
-            chain {
-                id
-                network
-            }
-            state {
-                id
-                apy
-                netApy
-                totalAssets
-                totalAssetsUsd
-                fee
-                timelock
-                allocation {
-                    market {
-                    uniqueKey
-                    loanAsset {
-                        name
-                        symbol
-                    }
-                    collateralAsset {
-                        name
-                        symbol
-                    }
+            decimals
+          }
+          chain {
+            id
+            network
+          }
+          state {
+            id
+            apy
+            netApy
+            totalAssets
+            totalAssetsUsd
+            fee
+            timelock
+            allocation {
+              market {
+                uniqueKey
+                loanAsset {
+                  name
+                  symbol
+                }
+                collateralAsset {
+                  name
+                  symbol
+                }
                 oracleAddress
                 irmAddress
                 lltv
-                }
-                supplyCap
-                supplyAssets
-                supplyAssetsUsd
-                }
+              }
+              supplyCap
+              supplyAssets
+              supplyAssetsUsd
             }
-            }
+          }
         }
+      }
     }
     """
 
@@ -161,44 +160,42 @@ def fetch_data_metamorpho():
     if response.status_code == 200:
         data = response.json().get("data", {}).get("vaults", {}).get("items", [])
         with app.app_context():
-
             for market in data:
-                if market["state"]["totalAssetsUsd"]:
-                    try:
+                try:
+                    if market and market["state"] and market["state"].get("totalAssetsUsd"):
                         protocol = "MetaMorpho"
-                        supply_token = market["state"]["allocation"][0]["market"]["loanAsset"]["symbol"]
-                        supply_apy = market["state"]["apy"]
-                        # collateral = market["allocation"]["market"]["collateralAsset"]["symbol"]
+                        if market["state"].get("allocation") and market["state"]["allocation"][0].get("market"):
+                            supply_token = market["state"]["allocation"][0]["market"]["loanAsset"]["symbol"]
+                        else:
+                            continue
+                        supply_apy = market["state"].get("apy", 0)
                         chain = market["chain"]["network"]
-                        supply_amount = market["state"]["totalAssetsUsd"]
-
-                        print(f" {protocol} {supply_token} / {chain} {supply_apy} {supply_amount}")
+                        supply_amount = market["state"].get("totalAssetsUsd", 0)
 
                         rate = MoneyMarketRate(
-                                token=supply_token,
-                                collateral="",
-                                protocol=protocol,
-                                liquidity_rate=supply_apy*100,
-                                liquidity_reward_rate=None,
-                                chain=chain.capitalize(),
-                                borrow_rate=0,
-                                tvl=supply_amount,
-                                timestamp=datetime.utcnow(),
-                            )
+                            token=supply_token,
+                            collateral="",
+                            protocol=protocol,
+                            liquidity_rate=supply_apy * 100,
+                            liquidity_reward_rate=None,
+                            chain=chain.capitalize(),
+                            borrow_rate=0,
+                            tvl=supply_amount,
+                            timestamp=datetime.utcnow(),
+                        )
 
                         db.session.add(rate)
-
-                    except KeyError as e:
-                        print(f"{e}")
+                except Exception as e:
+                    logger.error(f"Error processing vault data: {e}")
 
             db.session.commit()
 
     else:
-        print(f"Query failed to run with a {response.status_code}.")
+        logger.error(f"Query failed to run with a {response.status_code}.")
 
-def fetch_data_morpho():
-    fetch_data()
+def fetch_data():
+    fetch_data_morpho()
     fetch_data_metamorpho()
 
 if __name__ == '__main__':
-    fetch_data_morpho()
+    fetch_data()
