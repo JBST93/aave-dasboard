1f3bc4b4d8d2d070714af6942d699d45412ac10b
Jacek Bastin <jacek.bastin@gmail.com>
2024-07-27 00:35:27 +0100

    Fixing migration file

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: 'https://defi-dashboard-99d015fc546e.herokuapp.com/api',
 });
 
 export default instance;
