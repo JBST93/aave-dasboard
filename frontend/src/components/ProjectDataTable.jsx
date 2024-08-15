d9addc6f502a6073b13a8974c2e3ca768533bf4e
Jacek Bastin <jacek.bastin@gmail.com>
2024-08-15 17:43:58 +0100

    Curve Pool

--- frontend/src/components/ProjectDataTable.jsx
+++ frontend/src/components/ProjectDataTable.jsx
@@ -2,7 +2,6 @@ import { DataGrid } from '@mui/x-data-grid';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import '../App.css';
 
 const DataTable = ({ rows }) => {
   const validRows = Array.isArray(rows) ? rows : [];
