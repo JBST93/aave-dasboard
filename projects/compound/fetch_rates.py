2e9c9399409be4d157b6a74ed77c6b2c9211fcbc
Jacek Bastin <jacek.bastin@gmail.com>
2024-08-25 00:55:50 +0100

    Compound USD

--- projects/compound/fetch_rates.py
+++ projects/compound/fetch_rates.py
@@ -14,6 +14,7 @@ load_dotenv(os.path.join(project_root, '.env'))
 # Import app and db from the root directory
 from app import app, db
 from instances.YieldRate import YieldRate as Data
+from utils.get_price import get_price
 
 # Construct the absolute path to the aave_abi.json file
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -23,37 +24,50 @@ contracts= [
     {
         "token":"USDC",
         "address":"0xc3d688B66703497DAA19211EEdff47f25384cdc3",
-        "chain":"ethereum"
+        "chain":"ethereum",
+        "decimals":6
     },
     {
         "token": "USDT",
         "address":"0x3Afdc9BCA9213A35503b077a6072F3D0d5AB0840",
-        "chain": "ethereum"
+        "chain": "ethereum",
+        "decimals":6
+
     },
     {
         "token": "USDC",
         "address":"0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf",
-        "chain": "arbitrum"
+        "chain": "arbitrum",
+        "decimals":6
+
     },
     {
         "token": "USDC",
         "address":"0x2e44e174f7D53F0212823acC11C01A11d58c5bCB",
-        "chain": "optimism"
+        "chain": "optimism",
+        "decimals":6
+
     },
     {
         "token": "USDT",
         "address": "0xd98Be00b5D27fc98112BdE293e487f8D4cA57d07",
-        "chain": "arbitrum"
+        "chain": "arbitrum",
+        "decimals":6
+
     },
     {
         "token":"USDT",
         "address":"0x2e44e174f7D53F0212823acC11C01A11d58c5bCB",
-        "chain":"optimism"
+        "chain":"optimism",
+        "decimals":6
+
     },
     {
         "token":"wETH",
         "address":"0xA17581A9E3356d9A858b789D68B4d866e593aE94",
-        "chain":"ethereum"
+        "chain":"ethereum",
+        "decimals":18
+
     }
 
 
@@ -75,7 +89,10 @@ def fetch_store_rates():
             market = contract["token"]
             address = contract["address"]
             chain = contract["chain"]
+            decimals = contract["decimals"]
+
             comp_price = get_comp_price()
+            eth_price = get_price("ETH","","")
 
 
             abi_path = os.path.join(script_dir, f'compound_abi_{chain}.json')
@@ -104,7 +121,12 @@ def fetch_store_rates():
 
                 # Total supply
                 tvl = pool_contract.functions.totalSupply().call()
-                tvl_transformed = tvl / 1e6
+                tvl_transformed = tvl / decimals
+
+                if market == "wETH":
+                    tvl_usd = float(tvl_transformed) * eth_price
+                else:
+                     tvl_usd = float(tvl_transformed)
 
                 baseTrackingSupplySpeed = float(pool_contract.functions.baseTrackingBorrowSpeed().call())
                 trackingIndexScale = float(pool_contract.functions.trackingIndexScale().call())
@@ -119,7 +141,7 @@ def fetch_store_rates():
                     yield_rate_base=float(apy_base_formatted),
                     yield_rate_reward=reward_apy,
                     yield_token_reward="COMP",
-                    tvl=tvl_transformed,
+                    tvl=tvl_usd,
                     chain=chain.capitalize(),
                     type='',
                     smart_contract=address,
