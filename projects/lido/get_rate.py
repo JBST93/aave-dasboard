0545147c74d0ee642e62d98893bf9c2553299649
Jacek Bastin <jacek.bastin@gmail.com>
2024-08-26 20:41:29 +0100

    Fix

--- projects/lido/get_rate.py
+++ projects/lido/get_rate.py
@@ -9,62 +9,59 @@ sys.path.append(project_root)
 load_dotenv(os.path.join(project_root, '.env'))
 
 from app import app, db
-from scripts.utils import load_abi, get_curve_price
+from scripts.utils import load_abi
 from instances.TokenData import TokenData as Data
+from utils.get_infura import select_infura
+from utils.get_price import get_price
 
 
-provider_abi = load_abi("lido",'lido_abi.json')
-
-infura_url = "https://mainnet.infura.io/v3/"
-
-infura_key = os.getenv('INFURA_KEY')
-if not infura_key:
-    raise ValueError("INFURA_KEY not found in environment variables")
-
-def get_curve_price(address):
-    endpoint = f"https://prices.curve.fi/v1/usd_price/ethereum/{address}"
-    try:
-        r = requests.get(endpoint, timeout=10)
-        r.raise_for_status()
-        data = r.json()
-        price = data.get("data", {}).get("usd_price")
-        return float(price)
-    except requests.RequestException as e:
-        print(f"Error fetching price from Curve: {e}")
-    return None
+token_list = [
+    {
+        "token":"stETH",
+        "decimals": 18,
+        "endpoint":"https://eth-api.lido.fi/v1/protocol/steth/apr/sma",
+        "chain":"ethereum",
+        "provider_abi": load_abi("lido",'steth_abi.json')
+    },
+    {
+        "token":"wstETH",
+        "address":"0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
+        "decimals": 18,
+        "chain":"ethereum",
+        "provider_abi": load_abi("lido",'wsteth_abi.json'),
+    }
+]
 
 
 def get_data_steth():
-    endpoint = "https://eth-api.lido.fi/v1/protocol/steth/apr/sma"
-    r = requests.get(endpoint)
-    data = r.json()
+    for token in token_list:
+        if token["endpoint"]:
+            r = requests.get(token["endpoint"])
+            data = r.json()
 
-    apr = round(data.get("data").get("aprs")[-1].get("apr"),2)
-    symbol = data.get("meta").get("symbol")
-    address = data.get("meta").get("address")
-    decimals = 18
+            apr = round(data.get("data").get("aprs")[-1].get("apr"),2)
+            address = data.get("meta").get("address")
 
-    web3 = Web3(Web3.HTTPProvider(infura_url + infura_key))
-    pool_contract = web3.eth.contract(address=address, abi=provider_abi)
+        web3 = select_infura(token["chain"])
+        pool_contract = web3.eth.contract(address=address, abi=token["provider_abi"])
 
-    supply_raw = pool_contract.functions.totalSupply().call()
-    supply = round(supply_raw/10**decimals,2)
+        supply_raw = pool_contract.functions.totalSupply().call()
+        supply = round(supply_raw/10**token["decimals"],2)
 
-    price = (get_curve_price(address))
-    tot_supply_usd = round(supply * price,2)
-    circ_supply_usd = round(supply * price,2)
+        price = get_price(address)
 
+        data = Data (
+            token= "stETH",
+            price= price,
+            price_source = "",
+            tot_supply= supply,
+            circ_supply= supply ,
+            timestamp=datetime.utcnow(),
+        )
 
-    data = Data (
-        token= "stETH",
-        price= price,
-        price_source= "Curve",
-        tot_supply= supply,
-        circ_supply= supply ,
-        timestamp=datetime.utcnow(),
-    )
+        db.session.add(data)
+        print(data)
 
-    db.session.add(data)
     db.session.commit()
 
 
@@ -73,7 +70,3 @@ def get_data_steth():
 # The exact fee size is defined by the DAO and can be changed in the future via DAO voting.
 # To collect the fee, the protocol mints new stETH token shares and assigns them to the fee recipients.
 # Currently, the fee collected by Lido protocol is 10% of staking rewards with half of it going to the node operators and the other half going to the protocol treasury.
-
-if __name__ == '__main__':
-    with app.app_context():
-        get_data_steth()
